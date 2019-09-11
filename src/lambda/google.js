65ac8c4995280677ae0f5021128f624c54d9c255
Igor Makarov <igormaka@gmail.com>
2019-09-11 10:18:22 +0300

    google it

--- src/lambda/google.js
+++ src/lambda/google.js
@@ -1,6 +1,16 @@
+const googleIt = require('google-it')
+
 exports.handler = async (event, context) => {
+  // Only allow POST
+  if (!event.queryStringParameters.query) {
+    return { statusCode: 405, body: "Empty not allowed" };
+  }
+
+  const results = await googleIt({'query': event.queryStringParameters.query})
+  const resultsArray = results.map(link => link['link'])
+
   return {
     statusCode: 200,
-    body: "Hello, World"
-  };
-};
+    body: JSON.stringify(resultsArray, null, 2)
+  }
+}
